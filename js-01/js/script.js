2223e07253b422ebce03bc5b795818dc81af98e3
Jean Paul Ferreira <jeanpaul@jepafe.net>
2022-09-20 15:19:51 -0300

    JS - 01

---
+++ js-01/js/script.js
@@ -0,0 +1,23 @@
+// alert('Hola');
+
+// let nombre = prompt('Ingrese su numbre: ');
+// alert('Su nombre es: ' + nombre);
+// console.log(nombre);
+
+// let nombre = 'Juan';
+// // var nombre;
+// // nombre = 'Maria';
+
+// // var nombre = 'Mac';
+
+// if (true) {
+//     let nombre = 'Maria';
+//     console.log(nombre);
+// }
+
+// console.log(nombre);
+
+// const PI = 3.1416;
+// const apellido = 'Perez';
+
+let numero = '123e10';
