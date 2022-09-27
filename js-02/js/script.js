13bc88270e3790efb040c010b58dae8eeeb81862
Jean Paul Ferreira <jeanpaul@jepafe.net>
2022-09-27 12:17:55 -0300

    JS - 02

---
+++ js-02/js/script.js
@@ -0,0 +1,64 @@
+// Operadores aritméticos
+
+const num1 = 10;
+const num2 = 20;
+
+let resultado;
+
+// resultado = 15 % 3;
+
+// resultado = num1;
+// resultado = resultado + num2;
+// resultado += num2;
+
+// resultado = 2;
+// resultado **= 3;
+
+let edad = 11;
+// resultado = edad >= 13;
+
+// let num = 10;
+// resultado = ++num;
+
+let pais = 'Brasil';
+
+// resultado = num1 < 20 && num2 >= 20;
+// resultado = edad >= 13 || pais == 'Argentina';
+
+// console.log(resultado);
+
+// if (edad > 13) {
+//     console.log('Puedes registrarte');
+// } else if (edad == 13) {
+//     console.log('Pedile permiso a tu padres');
+// } else {
+//     console.log('No puede registrarse');
+// }
+
+// switch (edad) {
+//     case 1:
+//     case 9:
+//     case 10:
+//     case 11:
+//     case 12:
+//         console.log('No puede registrarse');
+//         break;
+//     case 13:
+//         console.log('Pedile permiso a tu padres');
+//         break;
+//     default:
+//         console.log('Puedes registrarte');
+//         break;
+// }
+
+// let mensaje;
+
+// if (edad >= 13) {
+//     mensaje = 'Te puede registrar';
+// } else {
+//     mensaje = 'No te puede registrar';
+// }
+
+let mensaje = edad >= 13 ? 'Te puede registrar' : 'No te puede registrar';
+
+console.log(mensaje);
